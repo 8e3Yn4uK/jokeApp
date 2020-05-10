f0a805783240dbf6a5650dc28f54bf79805bd8a7
Oleksandr Kondratenko <sashko.kondratenko@gmail.com>
2020-05-10 21:33:41 +0300

    Initial commit

---
+++ src/test/java/org/oko/joke/demo/JokeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oko.joke.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
